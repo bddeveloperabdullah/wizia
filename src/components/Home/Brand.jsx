d5dc813379963b066630eeb7d7f47b735605fd9e
bddeveloperabdullah <bddeveloperabdullah@gmail.com>
2025-03-24 12:18:23 +0600

    Brand Section Done

--- src/components/Home/Brand.jsx
+++ src/components/Home/Brand.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <>
+      <section className="bg-[#07292F] text-white pt-8 pb-10 -mt-0.5">
+        <div className="container mx-auto">
+          <div>
+            <h3 className="text-center font-montserrat uppercase text-secondary">
+              our trusted partners
+            </h3>
+            <div className="flex flex-wrap justify-center items-center gap-5 mt-7">
+              <img src="/BackHub logo.png" alt="" />
+              <img src="/CableLabs logo.png" alt="" />
+              <img src="/DBS logo.png" alt="" />
+              <img src="/EasyEuro logo.png" alt="" />
+              <img src="/AMD logo.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Brand;
