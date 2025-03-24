58e395e850f27d48e8d704677a6b750e2a696b75
bddeveloperabdullah <bddeveloperabdullah@gmail.com>
2025-03-24 23:22:51 +0600

    Full Project Done & Slider Full Done & Mobild Slide Swipe Done

--- src/components/Home/Counter.jsx
+++ src/components/Home/Counter.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
+const counterItem = [
+  {
+    counter: "32%",
+    title: "Improvement in Open Rates",
+  },
+  {
+    counter: "75%",
+    title: "Improvement in Ramp Time",
+  },
+  {
+    counter: "35%",
+    title: "Improvement in Meetings Booked",
+  },
+];
+
 const Counter = () => {
-  return <div>Counter</div>;
+  return (
+    <>
+      <section className="px-5">
+        <div className="container mx-auto">
+          <div className="bg-[url('/mobileBg2.png')] md:bg-[url('/BG2.png')] bg-center bg-cover bg-no-repeat p-10 lg:p-20 rounded-2xl">
+            <div className="space-y-3.5">
+              <h2 className="w-[279px] lg:w-[591px] text-[28px] lg:text-[32px] text-white font-montserrat font-semibold">
+                Allocate effort where your reps make an inpact.
+              </h2>
+              <h3 className="text-2xl lg:text-[32px] text-secondary italic font-montserrat font-[500]">
+                Let us handle the rest.
+              </h3>
+              <p className="w-[279px] lg:w-[582px] text-lg md:text-xl text-white font-worksans font-light">
+                Keep your reps “in the air” -- out in the field and on the phone
+                where they can build relationships.
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row justify-start gap-5 md:gap-10 mt-10 md:mt-20">
+              {counterItem.map((item, index) => (
+                <div key={index}>
+                  <h2 className="text-[32px] lg:text-[40px] text-secondary font-montserrat font-bold">
+                    {item.counter}
+                  </h2>
+                  <p className="w-full md:w-[174px] text-lg md:text-xl text-[#E9EEF1] font-montserrat font-[500]">
+                    {item.title}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Counter;
--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,51 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSolidQuoteAltLeft } from "react-icons/bi";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { RiDoubleQuotesL } from "react-icons/ri";
+import { useSwipeable } from "react-swipeable";
+
+const sliderItem = [
+  {
+    id: 1,
+    text: "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+    userName: "Abdullah",
+    userInfo: "abdullah@gmail.com",
+  },
+  {
+    id: 2,
+    text: "Donec auctor turpis at nisi vulputate, non pretium risus sagittis. Nulla facilisi. Etiam varius leo ac est dapibus luctus.",
+    userName: "Shihab",
+    userInfo: "shihab@gmail.com",
+  },
+  {
+    id: 3,
+    text: "Curabitur tempor ut lectus nec varius. Fusce feugiat libero ac mauris volutpat, in gravida sem pulvinar.",
+    userName: "Shahin",
+    userInfo: "shahin@gmail.com",
+  },
+  {
+    id: 4,
+    text: "Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames.",
+    userName: "Jousna",
+    userInfo: "jousna@gmail.com",
+  },
+  {
+    id: 5,
+    text: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Duis aliquet egestas purus in blandit.",
+    userName: "Siam",
+    userInfo: "siam@gmail.com",
+  },
+];
 
 const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentSlider = sliderItem[currentIndex];
+
+  const handlePrev = () => {
+    if (currentIndex > 0) setCurrentIndex(currentIndex - 1);
+  };
+
+  const handleNext = () => {
+    if (currentIndex < sliderItem.length - 1) setCurrentIndex(currentIndex + 1);
+  };
+
+  const handelDotClick = (el) => {
+    setCurrentIndex(el);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: handleNext,
+    onSwipedRight: handlePrev,
+    preventScrollOnSwipe: true,
+    trackMouse: true,
+  });
+
   return (
     <>
       <section className="py-24 px-10">
         <div className="container mx-auto">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center gap-5">
+            {/* Prev Arrow Btn  */}
             <div>
-              <div className="hidden size-10 rounded-md bg-[#96ACAF33] cursor-pointer md:flex justify-center items-center">
-                <FaArrowLeft className="text-xl text-[#96ACAF]" />
+              <div
+                className={`hidden size-10 rounded-md bg-[#96ACAF33] cursor-pointer md:flex justify-center items-center`}
+                onClick={handlePrev}
+              >
+                <FaArrowLeft
+                  className={`text-xl  ${
+                    currentIndex === 0 ? "text-[#96ACAF]" : "text-secondary"
+                  }`}
+                />
               </div>
             </div>
 
-            <div className="w-[920px] flex flex-col justify-center items-center gap-11">
-              <div className="size-[74px] rounded-full bg-[#183A40] flex justify-center items-center">
+            <div
+              {...handlers}
+              className="w-[920px] flex flex-col justify-center items-center gap-11"
+            >
+              <div className="size-[56px] lg:size-[74px] rounded-full bg-[#183A40] flex justify-center items-center">
                 <RiDoubleQuotesL className="text-4xl text-secondary" />
               </div>
-              <h3 className="text-3xl text-white text-center font-worksans font-light">
-                Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.
+              <h3 className=" text-2xl lg:text-3xl text-white text-center font-worksans font-light">
+                {currentSlider.text}
               </h3>
               <div className="text-center">
-                <h3 className="text-2xl text-secondary font-worksans font-semibold">
-                  Abdullah
+                <h3 className="text-lg lg:text-2xl text-secondary font-worksans font-semibold">
+                  {currentSlider.userName}
                 </h3>
                 <p className="text-xl text-[#96ACAF] font-worksans font-normal">
-                  abdullah@gmail.com
+                  {currentSlider.userInfo}
                 </p>
               </div>
             </div>
 
+            {/* Next Arrow Btn  */}
             <div>
-              <div className="hidden size-10 rounded-md bg-[#96ACAF33] cursor-pointer md:flex justify-center items-center">
-                <FaArrowRight className="text-xl text-secondary" />
+              <div
+                className="hidden size-10 rounded-md bg-[#96ACAF33] cursor-pointer md:flex justify-center items-center"
+                onClick={handleNext}
+              >
+                <FaArrowRight
+                  className={`text-xl ${
+                    currentIndex === sliderItem.length - 1
+                      ? " text-[#96ACAF]"
+                      : "text-secondary"
+                  }`}
+                />
               </div>
             </div>
           </div>
 
-          <div className="sliderBullet">
+          {/* Arrow Dot Btn  */}
+          <div>
             <div className="flex justify-center items-center gap-3.5 mt-10">
-              {Array.from(Array(5).keys()).map((el) => (
+              {Array.from(Array(sliderItem.length).keys()).map((el) => (
                 <div
-                  className="size-2.5 rounded-full bg-secondary cursor-pointer"
+                  className={`size-2.5 rounded-full cursor-pointer ${
+                    el === currentIndex ? "bg-secondary" : "bg-[#394648]"
+                  }`}
                   key={el}
+                  onClick={() => handelDotClick(el)}
                 />
               ))}
             </div>
