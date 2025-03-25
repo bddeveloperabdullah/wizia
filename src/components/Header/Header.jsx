743a5514b3d4f69539bb93997715facb73a0c9d8
bddeveloperabdullah <bddeveloperabdullah@gmail.com>
2025-03-25 11:31:29 +0600

    Mobile Menu Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,22 +3,10 @@ import Button from "../../similar/Button";
 import { IoIosClose, IoMdMenu } from "react-icons/io";
 
 const menuItem = [
-  {
-    itemName: "About Us",
-    url: "#",
-  },
-  {
-    itemName: "Pricing",
-    url: "#",
-  },
-  {
-    itemName: "Customers",
-    url: "#",
-  },
-  {
-    itemName: "Solutions",
-    url: "#",
-  },
+  { itemName: "About Us", url: "#" },
+  { itemName: "Pricing", url: "#" },
+  { itemName: "Customers", url: "#" },
+  { itemName: "Solutions", url: "#" },
 ];
 
 const Header = () => {
@@ -31,10 +19,11 @@ const Header = () => {
     <>
       <header className="bg-primary border-b border-borderColor py-6 px-5 lg:px-10">
         <div className="container mx-auto">
+          {/* Desktop Menu  */}
           <nav className="flex justify-between items-center">
             <div>
               <a href="#">
-                <img src="Logo.png" alt="" />
+                <img src="Logo.png" alt="Logo" />
               </a>
             </div>
             <div className="hidden md:flex gap-3.5 lg:gap-7">
@@ -59,10 +48,33 @@ const Header = () => {
                 className="md:hidden text-3xl text-white cursor-pointer"
                 onClick={handelToggleMenu}
               >
-                {menu ? <IoIosClose /> : <IoMdMenu />}
+                <IoMdMenu />
               </button>
             </div>
           </nav>
+
+          {/* Mobile Menu */}
+          <div
+            className={`fixed top-0 right-0 w-64 h-full bg-primary text-white transform ${
+              menu ? "translate-x-0" : "translate-x-full"
+            } transition-transform duration-300 ease-in-out md:hidden`}
+          >
+            <button
+              className="absolute top-5 right-5 text-3xl"
+              onClick={handelToggleMenu}
+            >
+              <IoIosClose />
+            </button>
+            <ul className="flex flex-col gap-5 p-10 mt-16">
+              {menuItem.map((item, index) => (
+                <li key={index}>
+                  <a href={item.url} className="text-lg">
+                    {item.itemName}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </header>
     </>
